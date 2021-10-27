eb96d773c883af854844841aa545ff9dde324287
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2021-10-26 17:53:20 -0700

    Lay groundwork for adding tests with access token

--- src/test/java/edu/ucla/library/iiif/auth/delegate/CantaloupeAuthDelegateIT.java
+++ src/test/java/edu/ucla/library/iiif/auth/delegate/CantaloupeAuthDelegateIT.java
@@ -79,13 +79,13 @@ public class CantaloupeAuthDelegateIT {
      ******/
 
     /**
-     * Tests the HTTP response of a request for a restricted image using Image API 2.
+     * Tests the HTTP response of a request without an Authorization header for a restricted image using Image API 2.
      *
      * @throws IOException If there is trouble reading the test file
      */
     @Test
-    public final void testResponseRestrictedV2() throws IOException, InterruptedException {
-        testResponse(RESTRICTED_IMAGE_ID, RESTRICTED_RESPONSE_TEMPLATE);
+    public final void testResponseRestrictedNoTokenV2() throws IOException, InterruptedException {
+        testResponse(RESTRICTED_IMAGE_ID, RESTRICTED_RESPONSE_TEMPLATE, null);
     }
 
     /**
@@ -95,7 +95,7 @@ public class CantaloupeAuthDelegateIT {
      */
     @Test
     public final void testResponseOpenV2() throws IOException, InterruptedException {
-        testResponse(OPEN_IMAGE_ID, OPEN_RESPONSE_TEMPLATE);
+        testResponse(OPEN_IMAGE_ID, OPEN_RESPONSE_TEMPLATE, null);
     }
 
     /******
@@ -103,13 +103,13 @@ public class CantaloupeAuthDelegateIT {
      ******/
 
     /**
-     * Tests the HTTP response of a request for a restricted image using Image API 3.
+     * Tests the HTTP response of a request without an Authorization header for a restricted image using Image API 3.
      *
      * @throws IOException If there is trouble reading the test file
      */
     @Test
-    public final void testResponseRestrictedV3() throws IOException, InterruptedException {
-        testResponse(RESTRICTED_IMAGE_ID, RESTRICTED_RESPONSE_TEMPLATE);
+    public final void testResponseRestrictedNoTokenV3() throws IOException, InterruptedException {
+        testResponse(RESTRICTED_IMAGE_ID, RESTRICTED_RESPONSE_TEMPLATE, null);
     }
 
     /**
@@ -119,38 +119,49 @@ public class CantaloupeAuthDelegateIT {
      */
     @Test
     public final void testResponseOpenV3() throws IOException, InterruptedException {
-        testResponse(OPEN_IMAGE_ID, OPEN_RESPONSE_TEMPLATE);
+        testResponse(OPEN_IMAGE_ID, OPEN_RESPONSE_TEMPLATE, null);
     }
 
     /**
-     * Tests the HTTP response of a request. We read the keys into a sorted map to be able to get a consistent
-     * representation (regardless of JSON formatting).
+     * Tests the HTTP response of a request.
      *
      * @param aImageID The identifier of the image whose info we're requesting
-     * @param aExpected An file with the expected JSON response
+     * @param aExpectedTemplate A file with the expected JSON response as a template, with slots for filling in URLs
+     * @param aToken An access token for authentication, to be added to the Cantaloupe request in an Authorization
+     *        header
      * @throws IOException If there is trouble reading the test file
      */
-    private void testResponse(final String aImageID, final File aExpected)
+    private void testResponse(final String aImageID, final File aExpectedTemplate, final String aToken)
             throws IOException, InterruptedException {
         final Map<String, String> envProperties = System.getenv();
+
         final String iiifURL = envProperties.get(TestConfig.IIIF_URL_PROPERTY);
         final String imageURL = StringUtils.format(IMAGE_URL_TEMPLATE, iiifURL, "2", aImageID);
         final URI requestURL = URI.create(imageURL + "/info.json");
-        final HttpRequest request = HttpRequest.newBuilder(requestURL).build();
-        final HttpResponse<String> response = HTTP.send(request, BodyHandlers.ofString());
-        final String[] urls;
+        final HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(requestURL);
+
+        final String[] templateURLs;
+        final String expected;
+
+        final HttpResponse<String> response;
 
         // If we have a restricted item, the Hauth services URLs also need to be added to the info.json
-        if (aExpected == RESTRICTED_RESPONSE_TEMPLATE) {
+        if (aExpectedTemplate == RESTRICTED_RESPONSE_TEMPLATE) {
             final String cookieServiceURL = envProperties.get(Config.AUTH_COOKIE_SERVICE);
             final String tokenServiceURL = envProperties.get(Config.AUTH_TOKEN_SERVICE);
 
-            urls = new String[] { imageURL, cookieServiceURL, tokenServiceURL };
+            templateURLs = new String[] { imageURL, cookieServiceURL, tokenServiceURL };
         } else {
-            urls = new String[] { imageURL };
+            templateURLs = new String[] { imageURL };
         }
+        expected = StringUtils.format(StringUtils.read(aExpectedTemplate), templateURLs);
 
-        TestUtils.assertEquals(StringUtils.format(StringUtils.read(aExpected), urls), response.body());
+        if (aToken != null) {
+            requestBuilder.header("Authorization", StringUtils.format("Bearer {}", aToken));
+        }
+        response = HTTP.send(requestBuilder.build(), BodyHandlers.ofString());
+
+        TestUtils.assertEquals(expected, response.body());
     }
 
 }
